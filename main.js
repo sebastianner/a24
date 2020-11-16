cf56682f1fc11c2bb45faa4a51332cd8cd2d94a4
sebastianner <sebastianner23@gmail.com>
2020-11-16 17:54:27 -0500

    movie names fetch ready

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// let baseUrl = 'https://api.themoviedb.org/3/'
-// let test = fetch('https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&sort_by=popularity.desc&include_adult=yes&include_video=yes&with_companies=41077')
+let test = fetch('https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&include_adult=yes&include_video=yes&with_companies=41077')
 
-// test
-//     .then(data=>data.json())
-//     .then(data=>{
-//         data.results.forEach(e => {
-//             var t = document.createElement('p')
-//             t.innerHTML = 'Hola'
-//             document.getElementById('features').appendChild(t)
-//         });
-//     })
+test
+    .then(data=>data.json())
+    .then(data=>{
+        data.results.forEach(e => {
+            var t = document.createElement('p')
+            t.innerHTML = e.title
+            document.getElementById('features').appendChild(t)
+        })
+    })
