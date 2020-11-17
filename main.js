21f578e14fe64a1a9cb8bc2adebbe1732a36151f
sebastianner <sebastianner23@gmail.com>
2020-11-17 10:32:17 -0500

    movie posters and catch ready

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-let test = fetch('https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&include_adult=yes&include_video=yes&with_companies=41077')
+let baseUrl = 'https://api.themoviedb.org/3/';
+let apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&include_adult=yes&include_video=yes&with_companies=41077'
+var counter = 0
 
-test
+
+let getConfig = function () {
+    fetch(apiUrl)
     .then(data=>data.json())
     .then(data=>{
+        console.log(data)
         data.results.forEach(e => {
-            var t = document.createElement('p')
-            t.innerHTML = e.title
-            document.getElementById('features').appendChild(t)
+            var movie = document.createElement('div')
+            movie.setAttribute('id','movie'+counter)
+            document.getElementById('feature-container').appendChild(movie)
+            var poster = document.createElement('img')
+            poster.setAttribute('src','https://image.tmdb.org/t/p/w342'+e.poster_path)
+            document.getElementById('movie'+counter).appendChild(poster)
+            var title = document.createElement('p')
+            title.innerHTML = e.title
+            document.getElementById('movie'+counter).appendChild(title)
+            counter++
         })
     })
+    .catch(err=>alert(err))
+}
+
+document.addEventListener('DOMContentLoaded',getConfig)
