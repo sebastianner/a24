7330f6f427a74b34c3e3c9d6752a6275c3b14c79
sebastianner <sebastianner23@gmail.com>
2020-11-20 11:42:38 -0500

    adding sass and gitignore

--- volume.js
+++ volume.js
@@ -5,10 +5,10 @@
     function mute(){
         if(video.muted === true){
             video.muted = false
-            btn.style.backgroundImage = "url(./static/mute.png)"
+            btn.style.backgroundImage = "url(/static/mute.png)"
         }
         else{
             video.muted = true
-            btn.style.backgroundImage = "url(./static/volume.png)"
+            btn.style.backgroundImage = "url(/static/volume.png)"
         }
     }
