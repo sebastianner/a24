4607a5631ad22cb61c3a37b3b596c253d2977490
sebastianner <sebastianner23@gmail.com>
2020-12-12 15:17:07 -0500

    js error handling,shop section added, fonts added.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 let baseUrl = 'https://api.themoviedb.org/3/';
 
-
 let getFeatures = function () {
     let getFeaturesUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&include_adult=yes&include_video=yes&with_companies=41077'
     let counter = 0
@@ -16,14 +15,12 @@ let getFeatures = function () {
             poster.setAttribute('src','https://image.tmdb.org/t/p/w342'+e.poster_path)
             poster.setAttribute('alt',e.title)
             document.getElementById('movie'+counter).appendChild(poster)
-            if (poster.height<513){
-                movie.remove()
-            }
             poster.onerror=function() {movie.style.display = 'none'}
             counter++
         })
+        removeErrors()
     })
-    .catch(err=>alert(err))
+    .catch(err=>console.log(err))
 }
 
 
@@ -45,9 +42,14 @@ let getUpcoming = ()=>{
             counter++
         });
     })
-    .catch(err=>alert(err))
+    .catch(err=>console.log(err))
 }
 
+function removeErrors() {
+    document.getElementById('movie1').style.display='none'
+    document.getElementById('movie8').style.display='none'
+    document.getElementById('movie12').style.display='none'
+}
 
 document.addEventListener('DOMContentLoaded',getFeatures)
 document.addEventListener('DOMContentLoaded',getUpcoming)
