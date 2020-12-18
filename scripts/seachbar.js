0ad1d1dbe631032e8f4cb21aaa0c4b84755f5f96
sebastianner <sebastianner23@gmail.com>
2020-12-18 13:20:12 -0500

    search functionality ready

--- scripts/seachbar.js
+++ scripts/seachbar.js
@@ -1,1 +1,48 @@
-// https://api.themoviedb.org/3/discover/movie?api_key=9660be5ceab38276fc0d0c4771d5c6be&language=en-US&include_adult=true&include_video=true&page=1&with_companies=41077
+let urlBase = 'https://api.themoviedb.org/3/'
+let apiKey = '9660be5ceab38276fc0d0c4771d5c6be'
+let searchBar = document.getElementById('search-bar')
+
+function toUpperCase(str) {
+    var arr = []
+    str.split(' ').forEach(e => {
+        arr.push(e.charAt(0).toUpperCase()+e.slice(1))
+    })
+    return arr.join(' ')
+}
+
+function runsearch(movies) {
+    console.log(movies)
+    searchBar.addEventListener('keyup',(e)=>{
+        let searchString = e.target.value
+        let filteredMovies = movies.results.filter(m=>{
+            return m.title.includes(toUpperCase(searchString))
+        })
+        console.log(filteredMovies)
+    })
+}
+
+
+let getConfig = function () {
+    let url = ''.concat(urlBase,'configuration?api_key=',apiKey)
+    fetch(url)
+    .then(result=>{return result.json()})
+    .then((data)=>{
+        baseImageURL = data.images.secure_base_url;
+        configData = data.images;
+        console.log('config:', data);
+        console.log('config fetched');
+        getMovies()
+    })
+    .catch(err=>console.log(err))
+}
+
+let getMovies = function () {
+    let url = ''.concat(urlBase,'discover/movie?api_key=',apiKey,'&page=1&with_companies=41077')
+    fetch(url)
+    .then(result=>result.json())
+    .then((data)=>{
+        runsearch(data)
+    })
+}
+
+getConfig()
