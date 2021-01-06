f5db2a472ffd4bef9e55b45aab45b0a14e0db38d
sebastianner <sebastianner23@gmail.com>
2021-01-06 13:41:07 -0500

    responsive menu hidding fixing

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,12 @@
-var menu = document.getElementById('menu-button')
-var menuText = document.getElementById('menu-text')
+let menu = document.getElementById('menu-button')
+let menuText = document.getElementById('menu-text')
+let e = document.getElementsByClassName('result-box')
 
 menu.addEventListener('click',()=>{
     if (menuText.style.display === 'none') {
         menuText.style.display = 'flex'
     }else{
+        e[1].style.display = 'none'
         menuText.style.display = 'none'
     }
 })
